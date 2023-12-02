5bfc09cc3ebcc998f2deb5c61a937798184eb337
suru1849 <suranjan1849@gmail.com>
2023-12-03 03:49:28 +0600

    accrodion edited

--- src/Pages/Home/Accrodion/Accrodion.jsx
+++ src/Pages/Home/Accrodion/Accrodion.jsx
@@ -1,36 +1,40 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionPanel,
+  AccordionTitle,
+} from "flowbite-react";
+
 const Accrodion = () => {
   return (
     <div className=" my-20">
       <h1 className="text-3xl font-bold text-center my-10">FAQ</h1>
-      <div className="join join-vertical w-full">
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" defaultChecked="checked" />
-          <div className="collapse-title text-xl font-medium">
-            What is our goal?
-          </div>
-          <div className="collapse-content">
-            <p>Teach you the best things</p>
-          </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            Is our teacheres are capable?
-          </div>
-          <div className="collapse-content">
-            <p>Yes,they are highly qualified</p>
-          </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            What will we provide you?
-          </div>
-          <div className="collapse-content">
-            <p>The things that you want.</p>
-          </div>
-        </div>
-      </div>
+      <Accordion collapseAll>
+        <AccordionPanel>
+          <AccordionTitle>What is our goal?</AccordionTitle>
+          <AccordionContent>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Provide you the best knowledge.
+            </p>
+          </AccordionContent>
+        </AccordionPanel>
+        <AccordionPanel>
+          <AccordionTitle>Is oue instructors are qualified?</AccordionTitle>
+          <AccordionContent>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Yes, our instructors are highly qualified.
+            </p>
+          </AccordionContent>
+        </AccordionPanel>
+        <AccordionPanel>
+          <AccordionTitle>What will we provide you?</AccordionTitle>
+          <AccordionContent>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Raw materials of any topic
+            </p>
+          </AccordionContent>
+        </AccordionPanel>
+      </Accordion>
     </div>
   );
 };
